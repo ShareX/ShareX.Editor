1438a51be0bb9f6a6897bd3e786ce94527f73a9b
Jaex <Jaex@users.noreply.github.com>
2026-01-27 03:49:42 +0300

    Fix swapped step fill and border color assignments
    Corrects the assignment of StepFillColor and StepBorderColor in MainViewModel for EditorTool.Step and EditorTool.Number. This ensures that fill and border colors are set and retrieved consistently.
    
    (cherry picked from commit fb7c7c931ea1a7452b2204b6e5564744d07350cc)

--- src/ShareX.Editor/ViewModels/MainViewModel.cs
+++ src/ShareX.Editor/ViewModels/MainViewModel.cs
@@ -280,7 +280,7 @@ namespace ShareX.Editor.ViewModels
             {
                 case EditorTool.Step:
                 case EditorTool.Number:
-                    Options.StepFillColor = color;
+                    Options.StepBorderColor = color;
                     break;
                 case EditorTool.Highlighter:
                     Options.HighlighterColor = color;
@@ -337,7 +337,7 @@ namespace ShareX.Editor.ViewModels
             {
                 case EditorTool.Step:
                 case EditorTool.Number:
-                    Options.StepBorderColor = color;
+                    Options.StepFillColor = color;
                     break;
                 default:
                     Options.FillColor = color;
@@ -517,8 +517,8 @@ namespace ShareX.Editor.ViewModels
 
                 case EditorTool.Step:
                 case EditorTool.Number:
-                    SelectedColorValue = Options.StepFillColor;
-                    FillColorValue = Options.StepBorderColor;
+                    SelectedColorValue = Options.StepBorderColor;
+                    FillColorValue = Options.StepFillColor;
                     StrokeWidth = Options.Thickness; // Or specific step thickness? EditorOptions uses generic Thickness.
                     ShadowEnabled = Options.Shadow;
                     FontSize = Options.FontSize;
